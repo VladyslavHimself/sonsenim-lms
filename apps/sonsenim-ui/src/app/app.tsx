dc642c5d8228a596ebebe8cde2657b4b5190bcc8
vladyslav-lutchyn <vladyslav.lutchyn@vitechteam.com>
2024-03-09 17:01:39 -0500

    Added tanstack router initial config

--- apps/sonsenim-ui/src/app/app.tsx
+++ apps/sonsenim-ui/src/app/app.tsx
@@ -1,6 +1,17 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { routeTree } from '../routeTree.gen';
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
 export function App() {
   return (
-    <h1>hello world</h1>
+    <RouterProvider router={router} />
   );
 }
 
--- apps/sonsenim-ui/vite.config.ts
+++ apps/sonsenim-ui/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 export default defineConfig({
   root: __dirname,
@@ -17,7 +18,12 @@ export default defineConfig({
     host: 'localhost',
   },
 
-  plugins: [react(), nxViteTsPaths()],
+  plugins: [react(), nxViteTsPaths(), TanStackRouterVite({
+    "routesDirectory": "./apps/sonsenim-ui/src/routes",
+    "generatedRouteTree": "./apps/sonsenim-ui/src/routeTree.gen.ts",
+    "routeFileIgnorePrefix": "-",
+    "quoteStyle": "single"
+  })],
 
   // Uncomment this if you are using workers.
   // worker: {
