0acc85c32d9f853ec5b69fe79260cba20059e59b
vladyslav-lutchyn <vladyslav.lutchyn@vitechteam.com>
2024-03-09 16:34:52 -0500

    Deleted redundant files

--- apps/sonsenim-ui/src/app/app.tsx
+++ apps/sonsenim-ui/src/app/app.tsx
@@ -1,13 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
-    <div>
-      <NxWelcome title="sonsenim-ui" />
-    </div>
+    <h1>hello world</h1>
   );
 }
 
