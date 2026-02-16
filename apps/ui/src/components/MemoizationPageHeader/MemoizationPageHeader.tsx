a2835e3f5fe8a17d56695cf52279721e7d0c34b6
vladyslavhimself <51461002+VladyslavHimself@users.noreply.github.com>
2026-02-15 22:17:41 -0500

    Fix minor bugs & improvements (#2)
    * Remove unused code, simplify AuthProvider logic, and refine navigation behaviors.
    
    * Set up environment variables for HYPERDRIVE; update compatibility date and comment out unused route configurations.
    
    * Update production environment name in wrangler.toml
    ---------
    
    Co-authored-by: vladyslav.lutchyn <v-hive@Vladyslavs-MacBook-Pro.local>

--- apps/ui/src/components/MemoizationPageHeader/MemoizationPageHeader.tsx
+++ apps/ui/src/components/MemoizationPageHeader/MemoizationPageHeader.tsx
@@ -10,6 +10,7 @@ import {IS_REGULAR_TEST} from "@/pages/Memoization/memoizationPage.constants.ts"
 export default function MemoizationPageHeader() {
     const navigate = useNavigate();
     const { deck, currentTestStage } = useMemoizationPageState();
+    const location = useNavigate();
 
     const headerTitle = currentTestStage === IS_REGULAR_TEST ? deck?.deckName : "Error Correction";
 
@@ -20,7 +21,16 @@ export default function MemoizationPageHeader() {
                 <House size={25} />
             </Button>
             <h2>{ headerTitle }</h2>
-            <Button>Finish</Button>
+            <Button onClick={ifGetBackToPage}>Finish</Button>
         </div>
     );
+    
+    function ifGetBackToPage() {
+        if (window.history.length > 2) {
+            location(-1);
+        } else {
+            navigate('/dashboard');
+        }
+    }
+
 }
