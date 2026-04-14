c3282471234b1c58f1a71659bcaecd01dbbeeb02
vladyslav.lutchyn <v-hive@Vladyslavs-MacBook-Pro.local>
2026-04-14 10:27:09 -0400

    Add `date` parameter to update progression history logic and queries

--- apps/ui/src/pages/Navigation/Navbar.tsx
+++ apps/ui/src/pages/Navigation/Navbar.tsx
@@ -3,17 +3,36 @@ import NavSidebar from "@/pages/Navigation/NavSidebar/NavSidebar.tsx";
 import useNavigationStatus from "@/pages/Navigation/NavSidebar/useNavigationStatus.tsx";
 import useMediaQuery from "@/hooks/useMediaQuery.ts";
 import MobileNavbar from "@/pages/Navigation/MobileNavbar/MobileNavbar.tsx";
+import React from "react";
+
+let deferredPrompt;
 
 export default function Navbar() {
     const isNavbarShouldBeShown = useNavigationStatus();
     const isMobileResolution = useMediaQuery("(max-width: 600px)");
     const Navigation = isMobileResolution ? MobileNavbar : NavSidebar;
 
+    React.useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        if (urlParams.get('install') === 'true') {
+            window.addEventListener('beforeinstallprompt', (e) => {
+                e.preventDefault();
+                deferredPrompt = e;
+                console.log('installfired');
+
+                if (urlParams.get('install') === 'true') {
+                    deferredPrompt.prompt();
+                }
+            });
+        }
+        return () => window.removeEventListener('beforeinstallprompt', (e) => deferredPrompt = e);
+
+    }, [])
 
     return (
         <>
-            {isNavbarShouldBeShown && <Navigation />}
-            <Outlet />
+            {isNavbarShouldBeShown && <Navigation/>}
+            <Outlet/>
         </>
     );
 };
